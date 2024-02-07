db239866a2dce640df52b60eac7238ea8c9fba45
Maraz Mia <43583025+MarazMia@users.noreply.github.com>
2024-02-07 15:35:20 -0600

    Add files via upload

---
+++ code1.js
@@ -0,0 +1,13 @@
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.status;
+}
+
+a = httpGet('http://xsslabelgg.com/profile/boby');
+
+if(a==200){
+alert('XSS');
+}
