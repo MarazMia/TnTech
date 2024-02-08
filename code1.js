03d185dd574076da7cc0f41aae2593c03c041134
Maraz Mia <43583025+MarazMia@users.noreply.github.com>
2024-02-07 20:39:37 -0600

    Update code1.js

--- code1.js
+++ code1.js
@@ -1,13 +1,14 @@
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.status;
-}
+// function httpGet(theUrl)
+// {
+//     var xmlHttp = new XMLHttpRequest();
+//     xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+//     xmlHttp.send( null );
+//     return xmlHttp.status;
+// }
 
-a = httpGet('http://xsslabelgg.com/profile/boby');
+// a = httpGet('http://xsslabelgg.com/profile/boby');
 
-if(a==200){
-alert('XSS');
-}
+// if(a==200){
+// alert('XSS');
+// }
+alert('XSS')
